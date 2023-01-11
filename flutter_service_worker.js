7568fac22c9a265512d3f0ec78a3dab8d135bd37
Jonas Magalhães Silva <80786958+jonasmagalhaes-8@users.noreply.github.com>
2023-01-11 08:56:21 -0300

    FIX Url da API

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7a7642f429a92622c15cc9c6e3b871dd",
-"/": "7a7642f429a92622c15cc9c6e3b871dd",
-"main.dart.js": "3c8299c933203070d8013fce4d08dbcd",
+"index.html": "641f244647dcd16330378ea969b6f525",
+"/": "641f244647dcd16330378ea969b6f525",
+"main.dart.js": "d2b54f1e9f2e9f84098534e92bca9e3a",
 "manifest.json": "0c8197e5c38b5b676e417192db0b150c",
 "version.json": "edee8470b8e500d4b602cba43743fb05"
 };
